0a9e3107de97973ed0aafbf0a969550e2341ad21
Arbab <arbab.arshad97@gmail.com>
2021-04-26 01:53:50 +0000

    26april2021

--- bat_eval/run_detector.py
+++ bat_eval/run_detector.py
@@ -24,7 +24,7 @@ def get_audio_files(ip_dir, sample_size):
         matches.append(str(path))
         iterator=iterator+1
         if (iterator>=sample_size):
-		    break
+            break
     return matches
 
 
@@ -95,17 +95,17 @@ def run_model(det, audio, file_dur, samp_rate, detection_thresh, max_num_calls=0
 if __name__ == "__main__":
 
     # params
-    detection_thresh = 0.95      # make this smaller if you want more calls
+    detection_thresh = 0.99      # make this smaller if you want more calls
     do_time_expansion = True       # if audio is already time expanded set this to False
     save_individual_results = False # if True will create an output for each file
     save_summary_result = True     # if True will create a single csv file with all results
 
     # load data
-    sample_size=1000                             # Number of files to read
+    sample_size=5                             # Number of files to read
     data_dir = '/media/rabi/Data/ThesisData/Bats audio records'                                   # this is the path to your audio files
-    op_ann_dir = '/home/rabi/Documents/Thesis/batdetect/bat_eval/results'                              # this where your results will be saved
+    op_ann_dir = '/media/rabi/Data/ThesisData/audio data analysis/audio-clustering/plots_26april'                              # this where your results will be saved
     op_ann_dir_ind = os.path.join(op_ann_dir, 'individual_results')  # this where individual results will be saved
-    op_file_name_total = os.path.join(op_ann_dir, 'results.csv')
+    op_file_name_total = os.path.join(op_ann_dir, 'results_15march.csv')
     if not os.path.isdir(op_ann_dir):
         os.makedirs(op_ann_dir)
     if save_individual_results and not os.path.isdir(op_ann_dir_ind):
